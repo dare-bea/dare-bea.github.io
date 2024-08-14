02dbeb017a1a43c029ea1277c129cb4f078a20f6
dare-bea <twopenaboys@gmail.com>
2024-08-13 22:00:20 -0400

    Split nav.js

--- nav.js
+++ nav.js
@@ -36,26 +36,3 @@ if (getCookie("cookies") == null) {
 } else {
     document.getElementById("header").innerHTML = navhtml;
 }
-
-const theme = document.getElementById('theme');
-const theme_select = document.getElementById('theme-select');
-if (getCookie("theme") != null) {
-    theme_select.value = getCookie("theme");
-}
-changeTheme()
-function changeTheme() {
-    var theme_url = theme_select.value;
-    if (getCookie("cookies") == "ok") {
-        document.cookie = "theme=" + theme_url + "; path=/; max-age=2419200";
-    }
-    if (theme_url == "os-default") {
-        if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-            theme_url = "/css/dark.css";
-        } else {
-            theme_url = "/css/light.css"
-        }
-    }
-    theme.setAttribute('href', theme_url);
-}
-
-window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', changeTheme);
