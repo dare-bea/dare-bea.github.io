6462a895e7b0b0e7ed69957bd05c98b4504fc5ef
dare-bea <twopenaboys@gmail.com>
2024-12-20 19:48:05 -0500

    easier to use
    control characters use unicode symbols or letter abbrevations in stack instead of messing with stack formatting
    timeout instead of intervals to allow for changing speeds
    status added (halted, running, etc.)
    formatting
    add fast mode check instead of being imprecise
    add `z` to clear console

--- esolang/esolang.js
+++ esolang/esolang.js
@@ -1,4 +1,4 @@
-function mod (a, b) {
+function mod(a, b) {
   return ((a % b) + b) % b;
 };
 
@@ -9,7 +9,7 @@ var STDOUT = "";
 FILE = ``;
 STDIN = ``;
 
-const escape_sequences = {
+const escapeSequences = {
   0: 0,
   b: 8,
   t: 9,
@@ -22,6 +22,18 @@ const escape_sequences = {
   '\\': 92
 };
 
+const controlCharacters = { 0x00: "␀", 0x01: "␁", 0x02: "␂", 0x03: "␃",
+  0x04: "␄", 0x05: "␅", 0x06: "␆", 0x07: "␇", 0x08: "␈", 0x09: "␉", 0x0A: "␊",
+  0x0B: "␋", 0x0C: "␌", 0x0D: "␍", 0x0E: "␎", 0x0F: "␏", 0x10: "␐", 0x11: "␑",
+  0x12: "␒", 0x13: "␓", 0x14: "␔", 0x15: "␕", 0x16: "␖", 0x17: "␗", 0x18: "␘",
+  0x19: "␙", 0x1A: "␚", 0x1B: "␛", 0x1C: "␜", 0x1D: "␝", 0x1E: "␞", 0x1F: "␟",
+  0x80: "PAD", 0x81: "HOP", 0x82: "BPH", 0x83: "NBH", 0x84: "IND",
+  0x85: "NEL", 0x86: "SSA", 0x87: "ESA", 0x88: "HTS", 0x89: "HTJ",
+  0x8A: "VTS", 0x8B: "PLD", 0x8C: "PLU", 0x8D: "RI", 0x8E: "SS2", 0x8F: "SS3",
+  0x90: "DCS", 0x91: "PU1", 0x92: "PU2", 0x93: "STS", 0x94: "CCH", 0x95: "MW",
+  0x96: "SPA", 0x97: "EPA", 0x98: "SOS", 0x99: "SGCI", 0x9A: "SCI",
+  0x9B: "CSI", 0x9C: "ST", 0x9D: "OSC", 0x9E: "PM", 0x9F: "APC" };
+
 var stack = {};
 var calls = [];
 var functions = {};
@@ -32,60 +44,50 @@ var i = 0;
 var repeatID;
 
 function run() {
-  // reset();
+  if (FILE !== document.getElementById('file').value || i >= FILE.length) {
+    reset();
+  }
+  document.getElementById('status').textContent = "Running";
   if (repeatID !== undefined) {
-    clearInterval(repeatID);
+    clearTimeout(repeatID);
     repeatID = undefined;
   }
-  document.getElementById('stdout').value = "Running...";
-  if (document.getElementById('speed').value >= 0) {
-    function nextStep () {
+  if (!document.getElementById('fastmode').checked) {
+    STDIN = document.getElementById('stdin').value;
+    function nextStep() {
       if (i < FILE.length && !(STDIN.length === 0 && FILE[i] === 'i')) {
         doStep();
+        document.getElementById('status').textContent = "Running";
+        repeatID = setTimeout(nextStep, document.getElementById('speed').value);
       } else {
-        clearInterval(repeatID);
-        repeatID = undefined;
-      }
-    }
-    repeatID = setInterval(nextStep, document.getElementById('speed').value);
-  } else {
-    /*STDIN = document.getElementById('stdin').value;
-    function nextStep () {
-      if (i < FILE.length && !(STDIN.length === 0 && FILE[i] === 'i')) {
-        step();
-      } else {
-        clearInterval(repeatID);
-        repeatID = undefined;
-        document.getElementById('stdin').value = STDIN;
-        document.getElementById('stdout').value = STDOUT;
-        document.getElementById('status').textContent = `BSP: ${bsp}  SP: ${sp}  IP: ${i}`;
-        var stackl1 = ""
-        var stackl2 = ""
-        for (var j = bsp; j < sp; j++) {
-          stackl1 += stack[j]+" "
-          stackl2 += String.fromCharCode(stack[j])
-          for (var _ = 0; _ < (stack[j].toString().length)-String.fromCharCode(stack[j]).length; _++) {
-            stackl2 += " "
-          }
-          stackl2 += " "
+        document.getElementById('status').textContent = "Not Running";
+        if (STDIN.length === 0 && FILE[i] === 'i') {
+          document.getElementById('status').textContent = "Halted (Waiting for Input)";
         }
-        document.getElementById('stack').value = stackl1 + "\n" + stackl2;
+        repeatID = undefined;
       }
     }
-    repeatID = setInterval(nextStep, 0);*/
+    nextStep();
+  } else {
     STDIN = document.getElementById('stdin').value;
     while (i < FILE.length && !(STDIN.length === 0 && FILE[i] === 'i')) {
       step();
     }
+    document.getElementById('status').textContent = "Not Running";
+    if (STDIN.length === 0 && FILE[i] === 'i') {
+      document.getElementById('status').textContent = "Halted (Waiting for Input)";
+    }
     document.getElementById('stdin').value = STDIN;
     document.getElementById('stdout').value = STDOUT;
-    document.getElementById('status').textContent = `BSP: ${bsp}  SP: ${sp}  IP: ${i}`;
+    document.getElementById('registers').textContent =
+      `BSP: ${bsp}  SP: ${sp}  IP: ${i}`;
     var stackl1 = ""
     var stackl2 = ""
     for (var j = bsp; j < sp; j++) {
-      stackl1 += stack[j]+" "
-      stackl2 += String.fromCharCode(stack[j])
-      for (var _ = 0; _ < (stack[j].toString().length)-String.fromCharCode(stack[j]).length; _++) {
+      stackl1 += stack[j] + " "
+      stackl2 += controlCharacters[stack[j]] ?? String.fromCharCode(stack[j])
+      for (var _ = 0; _ < (stack[j].toString().length) - String.fromCharCode(
+          stack[j]).length; _++) {
         stackl2 += " "
       }
       stackl2 += " "
@@ -94,19 +96,25 @@ function run() {
   }
 }
 
-function reset () {
-  clearInterval(repeatID);
-  repeatID = undefined;
+function reset() {
+  if (repeatID !== undefined){
+    clearTimeout(repeatID);
+    repeatID = undefined;
+  }
   stack = {};
   calls = [];
   functions = {};
-  bsp = 0; sp = 0; i = 0;
+  bsp = 0;
+  sp = 0;
+  i = 0;
   FILE = document.getElementById('file').value;
   STDIN = document.getElementById('stdin').value;
   STDOUT = "";
   document.getElementById('stdout').value = STDOUT;
-  document.getElementById('status').textContent = `BSP: ${bsp}  SP: ${sp}  IP: ${i}`;
+  document.getElementById('registers').textContent =
+    `BSP: ${bsp}  SP: ${sp}  IP: ${i}`;
   document.getElementById('stack').value = "";
+  document.getElementById('status').textContent = "Not Running";
 }
 
 /*
@@ -117,6 +125,15 @@ for (var i = bsp; i < sp; i++) {
 }
 */
 
+function stop() {
+  clearTimeout(repeatID);
+  repeatID = undefined;
+  document.getElementById('status').textContent = "Halted";
+  if (i >= FILE.length) {
+    document.getElementById('status').textContent = "Not Running";
+  }
+}
+
 function doStep() {
   STDIN = document.getElementById('stdin').value;
   if (i < FILE.length) {
@@ -124,18 +141,27 @@ function doStep() {
   }
   document.getElementById('stdin').value = STDIN;
   document.getElementById('stdout').value = STDOUT;
-  document.getElementById('status').textContent = `BSP: ${bsp}  SP: ${sp}  IP: ${i}`;
+  document.getElementById('registers').textContent =
+    `BSP: ${bsp}  SP: ${sp}  IP: ${i}`;
   var stackl1 = ""
   var stackl2 = ""
   for (var j = bsp; j < sp; j++) {
-    stackl1 += stack[j]+" "
-    stackl2 += String.fromCharCode(stack[j])
-    for (var _ = 0; _ < (stack[j].toString().length)-String.fromCharCode(stack[j]).length; _++) {
+    stackl1 += stack[j] + " "
+    stackl2 += controlCharacters[stack[j]] ?? String.fromCharCode(stack[j])
+    for (var _ = 0; _ < (stack[j].toString().length) - String.fromCharCode(
+        stack[j]).length; _++) {
       stackl2 += " "
     }
     stackl2 += " "
   }
   document.getElementById('stack').value = stackl1 + "\n" + stackl2;
+  document.getElementById('status').textContent = "Halted";
+  if (STDIN.length === 0 && FILE[i] === 'i') {
+    document.getElementById('status').textContent = "Halted (Waiting for Input)";
+  }
+  if (i >= FILE.length) {
+    document.getElementById('status').textContent = "Not Running";
+  }
 }
 
 function step() {
@@ -156,10 +182,10 @@ function step() {
   } else switch (FILE[i]) {
     case ',':
       break;
-    case 'x': 
+    case 'x':
       sp--;
       break;
-    case 'i': 
+    case 'i':
       if (STDIN.length) {
         stack[sp++] = STDIN.charCodeAt(0);
         STDIN = STDIN.substring(1);
@@ -171,7 +197,7 @@ function step() {
       STDOUT += String.fromCharCode(stack[--sp]);
       break;
     case 's':
-      [stack[sp-2], stack[sp-1]] = [stack[sp-1], stack[sp-2]]
+      [stack[sp - 2], stack[sp - 1]] = [stack[sp - 1], stack[sp - 2]]
       break;
     case 'r':
       var rotate = stack[--sp];
@@ -194,37 +220,63 @@ function step() {
       }
       break;
     case 'd':
-      stack[sp] = stack[sp-1];
+      stack[sp] = stack[sp - 1];
       sp++;
       break;
     case '+':
-      sp--; stack[sp-1] = stack[sp-1] + stack[sp]; break;
+      sp--;
+      stack[sp - 1] = stack[sp - 1] + stack[sp];
+      break;
     case '-':
-      sp--; stack[sp-1] = stack[sp-1] - stack[sp]; break;
+      sp--;
+      stack[sp - 1] = stack[sp - 1] - stack[sp];
+      break;
     case '*':
-      sp--; stack[sp-1] = stack[sp-1] * stack[sp]; break;
+      sp--;
+      stack[sp - 1] = stack[sp - 1] * stack[sp];
+      break;
     case '/':
-      sp--; stack[sp-1] = Math.floor(stack[sp-1] / stack[sp]); break;
+      sp--;
+      stack[sp - 1] = Math.floor(stack[sp - 1] / stack[sp]);
+      break;
     case '^':
-      sp--; stack[sp-1] = stack[sp-1] ** stack[sp]; break;
+      sp--;
+      stack[sp - 1] = stack[sp - 1] ** stack[sp];
+      break;
     case '%':
-      sp--; stack[sp-1] = mod(stack[sp-1], stack[sp]); break;
+      sp--;
+      stack[sp - 1] = mod(stack[sp - 1], stack[sp]);
+      break;
     case '&':
-      sp--; stack[sp-1] = stack[sp-1] & stack[sp]; break;
+      sp--;
+      stack[sp - 1] = stack[sp - 1] & stack[sp];
+      break;
     case '|':
-      sp--; stack[sp-1] = stack[sp-1] | stack[sp]; break;
+      sp--;
+      stack[sp - 1] = stack[sp - 1] | stack[sp];
+      break;
     case '\'':
-      sp--; stack[sp-1] = stack[sp-1] ^ stack[sp]; break;
+      sp--;
+      stack[sp - 1] = stack[sp - 1] ^ stack[sp];
+      break;
     case '~':
-      stack[sp-1] = -stack[sp-1]; break;
+      stack[sp - 1] = -stack[sp - 1];
+      break;
     case '!':
-      stack[sp-1] = ~stack[sp-1]; break;
+      stack[sp - 1] = ~stack[sp - 1];
+      break;
     case '=':
-      sp--; stack[sp-1] = stack[sp-1] === stack[sp] ? -1 : 0; break;
+      sp--;
+      stack[sp - 1] = stack[sp - 1] === stack[sp] ? -1 : 0;
+      break;
     case '>':
-      sp--; stack[sp-1] = stack[sp-1] > stack[sp] ? -1 : 0; break;
+      sp--;
+      stack[sp - 1] = stack[sp - 1] > stack[sp] ? -1 : 0;
+      break;
     case '<':
-      sp--; stack[sp-1] = stack[sp-1] < stack[sp] ? -1 : 0; break;
+      sp--;
+      stack[sp - 1] = stack[sp - 1] < stack[sp] ? -1 : 0;
+      break;
     case '[':
       if (stack[--sp]) {
         calls.push(i)
@@ -248,13 +300,18 @@ function step() {
       }
       break;
     case 'c':
-      stack[sp] = sp; sp++; break;
+      stack[sp] = sp;
+      sp++;
+      break;
     case 'b':
-      stack[sp++] = bsp; break;
+      stack[sp++] = bsp;
+      break;
     case 'C':
-      sp = stack[sp - 1]; break;
+      sp = stack[sp - 1];
+      break;
     case 'B':
-      bsp = stack[--sp]; break;
+      bsp = stack[--sp];
+      break;
     case '"':
       i++;
       while (FILE[i] !== '"') {
@@ -293,6 +350,9 @@ function step() {
       }
       calls.push(i);
       i = functions[name];
+      break;
+    case 'z':
+      STDOUT = "";
       break;
   }
   i++;
