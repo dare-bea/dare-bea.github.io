102d74cb1edc5cf0ea0d4c7f9001d822a28adfe4
dare-bea <twopenaboys@gmail.com>
2025-05-19 14:23:32 -0400

    Filters now get updated

--- wordgenerator/wordgeneratorpage.js
+++ wordgenerator/wordgeneratorpage.js
@@ -13,6 +13,60 @@ function updateScreen() {
   const patternInput = document.getElementById("patternInput");
   patternInput.value = pattern;
   
+  const filTable = document.getElementById("filters");
+  
+  // clear out any old rows
+  while (filTable.firstChild) {
+    filTable.removeChild(filTable.firstChild);
+  }
+
+  // rebuild the table
+  filters.forEach(([fil, rep], idx) => {
+    // create row and cells
+    const tr = document.createElement("tr");
+
+    // --- pattern input ---
+    const tdRegex = document.createElement("td");
+    const regexInput = document.createElement("input");
+    regexInput.type = "text";
+    regexInput.classList.add("filterRegexInput");
+    regexInput.value = fil.source; // DOM handles all escaping
+    regexInput.addEventListener('input', (e) => {
+      filters[idx][0] = new RegExp(e.target.value, "g");
+    });
+    tdRegex.appendChild(regexInput);
+    tr.appendChild(tdRegex);
+
+    // --- pattern input ---
+    const tdReplace = document.createElement("td");
+    const replaceInput = document.createElement("input");
+    replaceInput.type = "text";
+    replaceInput.classList.add("filterReplaceInput");
+    replaceInput.value = rep; // DOM handles all escaping
+    replaceInput.addEventListener('input', (e) => {
+      filters[idx][1] = e.target.value;
+    });
+    tdReplace.appendChild(replaceInput);
+    tr.appendChild(tdReplace);
+
+    // --- remove button ---
+    const tdRemove = document.createElement("td");
+    const btn = document.createElement("button");
+    btn.type = "button";
+    btn.classList.add("removeButton");
+    btn.textContent = "X";
+    btn.addEventListener('click', (e) => {
+      filters.splice(idx, 1);
+      updateScreen();
+    });
+    tdRemove.appendChild(btn);
+    tr.appendChild(tdRemove);
+
+    catTable.appendChild(tr);
+  });
+
+  
+  
   const catTable = document.getElementById("categories");
   
   // clear out any old rows
@@ -62,7 +116,7 @@ function updateScreen() {
     const tdRemove = document.createElement("td");
     const btn = document.createElement("button");
     btn.type = "button";
-    btn.classList.add("categoryRemoveButton");
+    btn.classList.add("removeButton");
     btn.textContent = "X";
     btn.addEventListener('click', (e) => {
       categories.splice(idx, 1);
