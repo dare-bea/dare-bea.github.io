1dcde01fab9d3c2a8edec59cee3e361a8891eca1
dare-bea <twopenaboys@gmail.com>
2025-10-27 13:54:54 -0400

    Improve GMC search query
    Added Wikipedia to sites allowed in search results.
    Prevented users from using reserved characters in the query.

--- tools/gmc/gmc.js
+++ tools/gmc/gmc.js
@@ -1,16 +1,14 @@
-function gimgwc() {
-  window.location.href = "https://www.google.com/search?tbm=isch&q="+document.getElementById('query').value+"%20site:commons.wikimedia.org";
-}
+const input = document.getElementById("query");
 
-var input = document.getElementById("query");
-
-// Execute a function when the user presses a key on the keyboard
 input.addEventListener("keypress", function(event) {
-  // If the user presses the "Enter" key on the keyboard
   if (event.key === "Enter") {
-    // Cancel the default action, if needed
     event.preventDefault();
-    // Trigger the button element with a click
     document.getElementById("search").click();
   }
 });
+
+const QUERY_APPEND = "(site:wikipedia.org OR site:commons.wikimedia.org)";
+
+function gimgwc() {
+  window.location.href = "https://www.google.com/search?tbm=isch&q=" + encodeURIComponent(document.getElementById('query').value + " " + QUERY_APPEND);
+}
