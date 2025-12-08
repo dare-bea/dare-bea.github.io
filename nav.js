52db5c515603409c4392372c30e77e1a9332f781
dare-bea <twopenaboys@gmail.com>
2025-12-08 10:27:39 -0500

    Theme & Settings

--- nav.js
+++ nav.js
@@ -21,10 +21,77 @@ function getCookie(cname) {
     return null;
 }
 
+/* NAV FUNCTIONS */
+
+const header = document.getElementById("header");
+const theme = document.getElementById('theme');
+let theme_select;
+
+function changeTheme() {
+    var theme_url = theme_select.value;
+    if (getCookie("cookies") == "ok") {
+        document.cookie = "theme=" + theme_url + "; path=/; max-age=2419200; SameSite=Lax";
+    }
+    if (theme_url == "os-default") {
+        if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+            theme_url = "/css/dark.css";
+        } else {
+            theme_url = "/css/light.css"
+        }
+    }
+    theme.setAttribute('href', theme_url);
+}
+
 function cookieNotice(cvalue) {
     document.cookie = 'cookies=' + cvalue + '; path=/; max-age=31536000; SameSite=Lax';
     if (cvalue == "ok") { changeTheme() };
     document.getElementById("cookie-notice").remove();
+}
+
+function pushThemeSelect () {
+    const themeSelectDOM = await getSourceAsDOM("/themeselect.html");
+    const navbar = document.getElementById('topnav');
+    for (const elem of themeSelectDOM.body.children) {
+        navbar.children.appendChild(elem);
+    }
+    theme_select = document.getElementById('theme-select');
+    if (getCookie("theme") != null) {
+        theme_select.value = getCookie("theme");
+    }
+    changeTheme()
+
+    window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', changeTheme);
+}
+
+const onSettingLoadFuncs = {
+    "cl-yuri": function (value) {
+        for (const elem of document.getElementsByClassName("yuri")) {
+            if (elem.tagName === "details") elem.open = value;
+        }
+    }
+}
+
+const onSettingChangeFuncs = {
+    "cl-yuri": function (value) {
+        window.location.reload();
+    }
+}
+
+function pushOptions (url) {
+    const DOM = await getSourceAsDOM(url);
+    const navbar = document.getElementById('topnav');
+    for (const elem of DOM.body.children) {
+        navbar.children.appendChild(elem);
+        if (!elem.id) continue;
+        const cookieValue = getCookie(elem.id);
+        if (cookieValue != null) {
+            elem.value = cookieValue;
+        }
+        elem.addEventListener("change", function (e) {
+            onSettingChangeFuncs[elem.id]?.(e.target.value)
+        });
+        onSettingLoadFuncs[elem.id]?.(elem.value);
+    }
 }
 
 async function loadNavigation() {
@@ -34,12 +101,15 @@ async function loadNavigation() {
     if (getCookie("cookies") == null) {
         const cookiesDOM = await getSourceAsDOM("/cookienotice.html");
         const cookieshtml = cookiesDOM.body.innerHTML;
-        document.getElementById("header").innerHTML = cookieshtml + navhtml;
+        header.innerHTML = cookieshtml + navhtml;
     } else {
-        document.getElementById("header").innerHTML = navhtml;
+        header.innerHTML = navhtml;
     }
 }
 
 // Call the async function
 loadNavigation()
-.then(runTheme);
+.then(function () {
+    if (header.classList.contains("cl-settings")) pushOptions("/clsettings.html");
+    if (!header.classList.contains("no-theme")) pushThemeSelect();
+});
